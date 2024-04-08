ca84919f2f38c5501f7ef7750962f99713b12720
sandro schutt <schuttsandro@outlook.com>
2024-04-08 15:18:54 -0300

    Adds plugin v1.0.0

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wp_Throw_Critical_Error
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
